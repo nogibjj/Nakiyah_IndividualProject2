a4e705f585fda02969f408d6b55a945100bc9aed
Nakiyah Dhariwala <nakiyahdhariwala@Nakiyahs-MacBook-Pro.local>
2024-11-01 14:51:05 -0400

    addedfunction

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use project2::{
-    clean_data, create_record, delete_record, load_data, query_data, query_specific_record,
+    extract_data, clean_data, create_record, delete_record, load_data, query_data, query_specific_record,
     update_record,
 };
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
+
+    let url = "https://raw.githubusercontent.com/viraterletska/Impact_of_Remote_Work_on_Mental_Health/main/data/Impact_of_Remote_Work_on_Mental_Health.csv";
+    let file_path = "Data/Impact_of_Remote_Work_on_Mental_Health.csv";
+
+    // Extract data from URL and save to file
+    match extract_data(url, file_path) {
+        Ok(downloaded_file_path) => {
+            println!("Data has been successfully downloaded to {}.", downloaded_file_path);
+        }
+        Err(e) => {
+            eprintln!("Failed to download data: {}", e);
+            return Err(e);
+        }
+    }
+
     let file_path = "Data/Impact_of_Remote_Work_on_Mental_Health.csv";
 
     // Extract, clean, and load data
